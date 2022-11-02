efc08c58b8d8ffe140b8a4aa1c4e95b8e1a7ea85
Nelesh Singla <nsingla@brandwatch.com>
2022-11-02 14:27:38 -0400

    initial draft of project to support parallel junit5 tests

---
+++ README.md
@@ -0,0 +1,9 @@
+# Basic Junit5 Framework
+This dependency allows you to run Junit5 tests in parallel, you just need to specify the number of parallel threads as `-DthreadCount=` as runtime param.
+
+## Requirement
+* Java 11
+* Maven >2.8
+
+## Building the project:
+* You can build the project with maven goals `clean install`
---
+++ src/main/java/com/brandwatch/junit5/TestWatcher.java
@@ -0,0 +1,39 @@
+package com.brandwatch.junit5;
+
+import com.brandwatch.junit5.utils.NamingUtils;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+import java.util.Optional;
+
+public class TestWatcher implements org.junit.jupiter.api.extension.TestWatcher {
+
+    private static final Logger logger = LoggerFactory.getLogger(TestWatcher.class);
+
+    @Override
+    public void testDisabled(ExtensionContext extensionContext, Optional<String> optional) {
+        logger.info("Test {} is disabled run.", NamingUtils.getTestName(extensionContext));
+    }
+
+    @Override
+    public void testSuccessful(ExtensionContext extensionContext) {
+        logger.info("Test {} succesfully run.", NamingUtils.getTestName(extensionContext));
+        MDC.remove("methodName");
+    }
+
+    @Override
+    public void testAborted(ExtensionContext extensionContext, Throwable throwable) {
+        logger.info("Test {} aborted.", NamingUtils.getTestName(extensionContext));
+        MDC.remove("methodName");
+    }
+
+    @Override
+    public void testFailed(ExtensionContext extensionContext, Throwable throwable) {
+        String testName = NamingUtils.getTestName(extensionContext);
+        MDC.put("methodName", testName);
+        logger.info("Test {} failed.", testName);
+        MDC.remove("methodName");
+    }
+}
